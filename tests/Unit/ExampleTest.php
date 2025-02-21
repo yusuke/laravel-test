0ed5a390c51b8fe4a6e8be5e7676a5e8d3ad8d19
Yusuke Yamamoto <yusuke@mac.com>
2025-02-22 08:32:08 +0900

    変更

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,7 +11,13 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
-        $this->assertTrue(strpos("Hello world", "hello"));
+        $this->assertFalse(strpos("Hello world", "hello"));
+
+
+        $this->assertFalse(strpos("hello world", "hello world"));
+
+
+        $this->assertFalse(strpos("hello world", "hello world"));
         $this->assertTrue(strpos("Hello world", "hello"));
         $this->assertTrue(strpos("Hello world", "hello"));
     }
